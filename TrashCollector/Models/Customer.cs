f8c5741cd5e620fa34c23b14b12e71181af847f8
Nate Poths <pothsn@gmail.com>
2019-06-25 15:02:52 -0500

    Created models for Employee and Customer, also created Employee and Customer controllers.

---
+++ TrashCollector/Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollector.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public int zipcode { get; set; }
+
+        
+
+    }
+}
---
+++ TrashCollector/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollector.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        public string name { get; set; }
+        public int zipcode { get; set; }
+
+        
+    }
+}
