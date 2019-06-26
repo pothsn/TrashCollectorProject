601b636d4c455452f159689dab0052355042c0fe
Nate Poths <pothsn@gmail.com>
2019-06-26 12:10:41 -0500

    In UserController, placed code in ActionResult Index and isAdminUser bool to check if the user is logged in/is an admin user.

--- TrashCollector/Controllers/UserController.cs
+++ TrashCollector/Controllers/UserController.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TrashCollector.Models;
 
 namespace TrashCollector.Controllers
 {
@@ -12,7 +15,45 @@ namespace TrashCollector.Controllers
         // GET: User
         public ActionResult Index()
         {
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = User.Identity;
+                ViewBag.Name = user.Name;
+
+                ViewBag.displayMenu = "No";
+
+                if (isAdminUser())
+                {
+                    ViewBag.displayMenu = "Yes";
+                }
+                return View();
+            }
+            else
+            {
+                ViewBag.Name = "Not Logged IN";
+            }
             return View();
+
         }
+
+        public bool isAdminUser()   
+        {   
+            if (User.Identity.IsAuthenticated)   
+            {   
+                var user = User.Identity;   
+                ApplicationDbContext context = new ApplicationDbContext();   
+                var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));   
+                var s = UserManager.GetRoles(user.GetUserId());   
+                if (s[0].ToString() == "Admin")   
+                {   
+                    return true;   
+                }   
+                else   
+                {   
+                    return false;   
+                }   
+            }   
+            return false;   
+        }  
     }
 }
